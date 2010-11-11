e43ee1443a21d069f89fd6f1b3945cf4e594ea0b
Michele Franzin <michele.franzin@gmail.com>
2010-11-11 12:30:13 +0100

    minor changes

--- src/main/java/org/vaadin/vol/client/ui/VOpenLayersMap.java
+++ src/main/java/org/vaadin/vol/client/ui/VOpenLayersMap.java
@@ -20,107 +20,101 @@ import com.vaadin.terminal.gwt.client.UIDL;
  * server are shown as HTML and mouse clicks are sent to the server.
  */
 public class VOpenLayersMap extends MapWidget implements Paintable {
-	
-	static MapOptions getDefaultMapOptions() {
-		MapOptions o  = new MapOptions();
+
+    static MapOptions getDefaultMapOptions() {
+        MapOptions o = new MapOptions();
 //		o.setNumZoomLevels(16);
-		return o;
-	}
-	
-	/** Set the CSS class name to allow styling. */
-	public static final String CLASSNAME = "v-openlayersmap";
+        return o;
+    }
+    /** Set the CSS class name to allow styling. */
+    public static final String CLASSNAME = "v-openlayersmap";
+    /** The client side widget identifier */
+    protected String paintableId;
+    /** Reference to the server connection object. */
+    protected ApplicationConnection client;
 
-	/** The client side widget identifier */
-	protected String paintableId;
+    /**
+     * The constructor should first call super() to initialize the component and
+     * then handle any initialization relevant to Vaadin.
+     */
+    public VOpenLayersMap() {
+        super("350px", "500px", getDefaultMapOptions());
 
-	/** Reference to the server connection object. */
-	protected ApplicationConnection client;
+        // This method call of the Paintable interface sets the component
+        // style name in DOM tree
+        setStyleName(CLASSNAME);
 
-	/**
-	 * The constructor should first call super() to initialize the component and
-	 * then handle any initialization relevant to Vaadin.
-	 */
-	public VOpenLayersMap() {
-		super("350px", "500px", getDefaultMapOptions());
+    }
 
-		// This method call of the Paintable interface sets the component
-		// style name in DOM tree
-		setStyleName(CLASSNAME);
+    /**
+     * Called whenever an update is received from the server
+     */
+    @Override
+    public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+        // This call should be made first.
+        // It handles sizes, captions, tooltips, etc. automatically.
+        if (client.updateComponent(this, uidl, true)) {
+            // If client.updateComponent returns true there has been no changes
+            // and we
+            // do not need to update anything.
+            return;
+        }
 
-	}
+        // Save reference to server connection object to be able to send
+        // user interaction later
+        this.client = client;
 
-	/**
-	 * Called whenever an update is received from the server
-	 */
-	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
-		// This call should be made first.
-		// It handles sizes, captions, tooltips, etc. automatically.
-		if (client.updateComponent(this, uidl, true)) {
-			// If client.updateComponent returns true there has been no changes
-			// and we
-			// do not need to update anything.
-			return;
-		}
+        // Save the client side identifier (paintable id) for the widget
+        paintableId = uidl.getId();
 
-		// Save reference to server connection object to be able to send
-		// user interaction later
-		this.client = client;
+        // TODO read zoom levels and coordinates
 
-		// Save the client side identifier (paintable id) for the widget
-		paintableId = uidl.getId();
-		
-		// TODO read zoom levels and coordinates
-		
-		// TODO should update existing layers instead of clearing and rebuilding
-		Layer[] layers = getMap().getLayers();
-		for (Layer layer : layers) {
-			getMap().removeLayer(layer);
-		}
-		
-		Iterator<Object> childIterator = uidl.getChildIterator();
-		while(childIterator.hasNext()) {
-			UIDL layer = (UIDL) childIterator.next();
-			String name = layer.getStringAttribute("name");
-			// TODO implement client side counterparts for map components
-			if(name != null && name.equals("metacarta")) {
-				//Defining a WMSLayer and adding it to a Map
-				WMSParams wmsParams = new WMSParams();
-				wmsParams.setFormat("image/png");
-				wmsParams.setLayers(layer.getStringAttribute("layer"));
-				wmsParams.setStyles("");
-				
-				WMS l = new WMS("Basic WMS", layer.getStringAttribute("uri"), wmsParams);
-				getMap().addLayer(l);
-			} else {
-				MarkerLayer markerLayer = new MarkerLayer(name);
-				markerLayer.updateFromUIDL(layer);
-				getMap().addLayer(markerLayer);
-			}
-		}
-	}
-	
-	
-	
-	class MarkerLayer extends Markers {
+        // TODO should update existing layers instead of clearing and rebuilding
+        Layer[] layers = getMap().getLayers();
+        for (Layer layer : layers) {
+            getMap().removeLayer(layer);
+        }
 
-		public MarkerLayer(String name) {
-			super(name);
-		}
+        Iterator<Object> childIterator = uidl.getChildIterator();
+        while (childIterator.hasNext()) {
+            UIDL layer = (UIDL) childIterator.next();
+            String name = layer.getStringAttribute("name");
+            // TODO implement client side counterparts for map components
+            if (name != null && name.equals("metacarta")) {
+                //Defining a WMSLayer and adding it to a Map
+                WMSParams wmsParams = new WMSParams();
+                wmsParams.setFormat("image/png");
+                wmsParams.setLayers(layer.getStringAttribute("layer"));
+                wmsParams.setStyles("");
 
-		public void updateFromUIDL(UIDL layer) {
-			int childCount = layer.getChildCount();
-			for(int i = 0; i < childCount; i++) {
-				UIDL childUIDL = layer.getChildUIDL(i);
-				double lon = childUIDL.getDoubleAttribute("lon");
-				double lat = childUIDL.getDoubleAttribute("lat");
-				Marker marker = new Marker(new LonLat(lon, lat));
-				addMarker(marker);
-				// TODO remove this when map has proper API
-				getMap().setCenter(new LonLat(lon, lat), 5);
-			}
-			
-		}
-		
-	}
+                WMS l = new WMS("Basic WMS", layer.getStringAttribute("uri"), wmsParams);
+                getMap().addLayer(l);
+            } else {
+                MarkerLayer markerLayer = new MarkerLayer(name);
+                markerLayer.updateFromUIDL(layer);
+                getMap().addLayer(markerLayer);
+            }
+        }
+    }
 
+    class MarkerLayer extends Markers {
+
+        public MarkerLayer(String name) {
+            super(name);
+        }
+
+        public void updateFromUIDL(UIDL layer) {
+            int childCount = layer.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                UIDL childUIDL = layer.getChildUIDL(i);
+                double lon = childUIDL.getDoubleAttribute("lon");
+                double lat = childUIDL.getDoubleAttribute("lat");
+                Marker marker = new Marker(new LonLat(lon, lat));
+                addMarker(marker);
+                // TODO remove this when map has proper API
+                getMap().setCenter(new LonLat(lon, lat), 5);
+            }
+
+        }
+    }
 }
